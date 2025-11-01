0960e16bbe212a0e99f90bc275604feca77a265b
Karpov20 <91140793+Karpov20@users.noreply.github.com>
2025-11-01 16:33:06 +0300

    Block3:Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "security/securitymanager.h"
 
 #include <QApplication>
 
@@ -6,6 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    security::installGuards();
     MainWindow window;
     window.show();
     return app.exec();
